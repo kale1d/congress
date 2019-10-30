cbcb9e2abeacbcb7ec7eb409a9bb3c54f01a2b3f
Denise Neustadt <denise.neustadt@synapse.com>
2019-10-30 11:57:28 -0300

    Add back button in detail component

--- src/containers/CongressPersonDetail/CongressPersonDetail.tsx
+++ src/containers/CongressPersonDetail/CongressPersonDetail.tsx
@@ -5,50 +5,47 @@ import "./CongressPersonDetailStyles.scss";
 
 type Props = RouteComponentProps;
 
-const CongressPersonDetail: FC<Props> = ({ location }) => {
+const CongressPersonDetail: FC<Props> = ({ location, history }) => {
   const memberInfo: CongressMember = location.state.data;
   console.log(memberInfo);
   return (
-    <div className="memberInfo">
-      <div className="memberInfo__mainInfo">
-        <div>
-          <h3>Personal Information</h3>
-          <span>Name: </span>
-          <span>
-            {memberInfo.first_name} {memberInfo.last_name}
-          </span>
-        </div>
-        <div>
-          <span>Title: </span> <span>{memberInfo.title}</span>
-        </div>
-        <div>
-          <span>Party: </span> <span>{memberInfo.party}</span>
-        </div>
-        <div>
-          <span>State: </span> <span>{memberInfo.state}</span>
-        </div>
+    <div>
+      <div onClick={() => history.goBack()} className="backButton">
+        Back
       </div>
-      <div className="memberInfo__contactInfo">
-        <h3>Contact Information</h3>
-        <div>
-          <span>URL: </span>{" "}
-          <span>
-            <a href={memberInfo.url} target="_blank">
-              {memberInfo.url}
-            </a>
-          </span>
+      <div className="memberInfo">
+        <div className="memberInfo__mainInfo">
+          <div>
+            <h3>Personal Information</h3>
+            <span>Name: </span>
+            <span>
+              {memberInfo.first_name} {memberInfo.last_name}
+            </span>
+          </div>
+          <div>
+            <span>Title: </span> <span>{memberInfo.title}</span>
+          </div>
+          <div>
+            <span>Party: </span> <span>{memberInfo.party}</span>
+          </div>
+          <div>
+            <span>State: </span> <span>{memberInfo.state}</span>
+          </div>
         </div>
-        <h4>Social Media</h4>
-        <div>
-          <span>
-            <img
-              src="../../utils/icons/youtube.png"
-              alt="Youtube"
-              width="42"
-              height="42"
-            />
-            Youtube
-          </span>
+        <div className="memberInfo__contactInfo">
+          <h3>Contact Information</h3>
+          <div>
+            <span>URL: </span>{" "}
+            <span>
+              <a href={memberInfo.url} target="_blank">
+                {memberInfo.url}
+              </a>
+            </span>
+          </div>
+          <h4>Social Media</h4>
+          <div>
+            <span>Youtube</span>
+          </div>
         </div>
       </div>
     </div>
